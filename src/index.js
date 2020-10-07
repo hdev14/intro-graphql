73ec66ae14458bd8b75e068e4b9c9598d17fd1ce
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-07 13:38:35 -0300

    feat: get fake users with graphql

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { resolve } = require('path')
+const { GraphQLServer } = require('graphql-yoga')
+const resolvers = require('./resolvers')
+
+const server = new GraphQLServer({
+    typeDefs: resolve(__dirname, 'schema.graphql'),
+    resolvers
+})
+
+server.start()
